af62dade638c4fdcc73921b8cf13ca5cc0e3ba47
coderwhy <coderwhy@gmail.com>
2022-10-26 08:56:51 +0800

    music_day01

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react'
+import { useRoutes, Link } from 'react-router-dom'
+import { useAppSelector, useAppDispatch, shallowEqualApp } from './store'
+import routes from './router'
+import { changeMessageAction } from './store/modules/counter'
+// import { IRootState } from './store'
+
+// import store from './store'
+
+// type GetStateFnType = typeof store.getState
+// type IRootState = ReturnType<GetStateFnType>
 
 function App() {
+  // const { count, message } = useSelector(
+  //   (state: IRootState) => ({
+  //     count: state.counter.count,
+  //     message: state.counter.message
+  //   }),
+  //   shallowEqual
+  // )
+
+  const { count, message } = useAppSelector(
+    (state) => ({
+      count: state.counter.count,
+      message: state.counter.message
+    }),
+    shallowEqualApp
+  )
+
+  /** 事件处理函数 */
+  const dispatch = useAppDispatch()
+  function handleChangeMessage() {
+    dispatch(changeMessageAction('呵呵呵呵呵'))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav">
+        <Link to="/discover">发现音乐</Link>
+        <Link to="/mine">我的音乐</Link>
+        <Link to="/focus">关注</Link>
+        <Link to="/download">下载客户端</Link>
+      </div>
+      <h2>当前计数: {count}</h2>
+      <h2>当前消息: {message}</h2>
+      <button onClick={handleChangeMessage}>修改message</button>
+      <Suspense fallback="">
+        <div className="main">{useRoutes(routes)}</div>
+      </Suspense>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/store/index.ts
@@ -0,0 +1,26 @@
+import { configureStore } from '@reduxjs/toolkit'
+import {
+  useSelector,
+  useDispatch,
+  TypedUseSelectorHook,
+  shallowEqual
+} from 'react-redux'
+
+import counterReducer from './modules/counter'
+
+const store = configureStore({
+  reducer: {
+    counter: counterReducer
+  }
+})
+
+type GetStateFnType = typeof store.getState
+type IRootState = ReturnType<GetStateFnType>
+type DispatchType = typeof store.dispatch
+
+// useAppSelector的hook
+export const useAppSelector: TypedUseSelectorHook<IRootState> = useSelector
+export const useAppDispatch: () => DispatchType = useDispatch
+export const shallowEqualApp = shallowEqual
+
+export default store
